433ec0080588e441dd7ed0597931f91c53e732b2
mkudmi <mku.dmi@gmail.com>
2026-04-19 15:31:55 +0300

    Refactor styles into modular CSS files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./styles/core.css";
+import "./styles/landing.css";
+import "./styles/dashboard.css";
+import "./styles/share-sheet.css";
+import "./styles/modals.css";
+import "./styles/not-found.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
